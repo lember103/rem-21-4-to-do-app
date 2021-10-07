3897e076975fc69f84d730351053a7ff6912876d
Max Lenz <maxlenz@mail.de>
2021-10-07 12:16:45 +0200

    add custom hooks

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,7 +4,8 @@ export default function Header() {
 
     return (
         <HeaderStyled>
-            <h1>Super Todo App</h1>
+            <StyledImage src={require("../icons/todo.png").default} alt={"todo icon"} width={30}/>
+            {/*<h1>Super Todo App</h1>*/}
         </HeaderStyled>
     )
 }
@@ -12,4 +13,12 @@ export default function Header() {
 const HeaderStyled = styled.header`
     text-align: center;
 
+`
+
+const StyledImage = styled.img`
+  width: 30px;
+  margin: 10px;
+  border: 2px solid black;
+  border-radius: 12px;
+  padding: 5px;
 `
